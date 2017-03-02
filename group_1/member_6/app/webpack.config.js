71516d330f510ca1d39f8c3d6e31a253f04ee2a1
dujingya <dujingya@shuzijiayuan.com>
2017-03-02 10:24:45 +0800

    dujingya

--- group_1/member_6/app/webpack.config.js
+++ group_1/member_6/app/webpack.config.js
@@ -1,3 +1,55 @@
 /**
  * Created by dujingya on 17/2/28.
  */
+
+var webpack=require("webpack");
+var CleanPlugin=require('clean-webpack-plugin');
+var webpackDevServer=require('webpack-dev-server');
+
+module.exports={
+    entry:"./src",
+    output:{
+        path:'./builds',
+        filename:"bundle.js",
+    },
+    module:{
+        loaders:[
+            {
+                test:/\.js/,
+                // exclude:/node_modules/,
+                loader:'babel-loader',
+                include:/src/
+            },
+            {
+                test:/\.scss/,
+                loaders:['style-loader','css-loader','sass-loader'],
+                include:/src/
+            },
+            {
+                test:/\.(png|gif|jpe?g|svg)/,
+                loader:'url?limit=10000'
+            },
+            {
+                test:/\.html/,
+                loader:'html-loader',
+            }
+
+        ]
+    },
+    plugins:[
+        new webpack.BannerPlugin('webpack教程实例'),
+        new webpack.optimize.UglifyJsPlugin({
+            compress:{
+                warnings:false
+            },
+            output:{
+                comments:false,
+            },
+        }),
+    ],
+    devServer:{
+        stats:{color:true},
+        port:8081,
+        contentBase:"builds"
+    }
+}
