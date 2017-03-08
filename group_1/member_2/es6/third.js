2a887f535968e71cda9e3554e43655302a2c1903
liuyun <liuyun@ihangmei.com>
2017-03-08 09:57:45 +0800

    the second week homework

--- group_1/member_2/es6/third.js
+++ group_1/member_2/es6/third.js
@@ -1,10 +1,12 @@
 // 写一个方法count，执行后在N秒后打印N，如在1秒后打印1，一直到10秒后
 let count = (n) => {
     let N = 1;
-    setInterval(()=>{
+    let timer = setInterval(()=>{
         if(N <= n){
             console.log(N++);
-        }
+        }else{
+            clearInterval(timer);
+    }
     }, 1000)
 };
 count(10);
