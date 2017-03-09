e211c079b35d6b882f4f696d02fa8eae409c4ece
charleszhang <charleszhang@jingbos-MBP.home>
2017-03-08 23:07:58 -0500

    homework2

---
+++ group_4/member_15/es6/first.js
@@ -0,0 +1,9 @@
+// 1.如下代码，写一个obj.sayName方法，执行obj.sayName()一秒后将obj.name打印出来
+let obj = {
+    name: 'zhufeng',
+    sayName(){
+      setTimeout(()=> console.log(this.name),1000)
+    }
+};
+
+obj.sayName();
---
+++ group_4/member_15/es6/third.js
@@ -0,0 +1,20 @@
+/*
+3.写一个方法count，执行后在N秒后打印N，如在1秒后打印1，一直到10秒后
+*/
+
+const MAX = 10;
+const MIN = 0;
+
+function count(num = 0){
+
+  if (num > MAX) {
+    return false
+  }else{
+    console.log(num)
+  }
+
+  setTimeout(()=> count(++num),1000)
+
+}
+
+count();
