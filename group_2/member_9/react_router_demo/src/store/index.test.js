e4be3f58b7748a3ad239b81b61899883d800d0e7
wangzhengbo <wangzhengbo1984@hotmail.com>
2017-03-20 20:32:59 +0800

    change toMatchObject to toEqual in test

--- group_2/member_9/react_router_demo/src/store/index.test.js
+++ group_2/member_9/react_router_demo/src/store/index.test.js
@@ -2,25 +2,25 @@ import React from 'react';
 import store from './index';
 
 describe('store', () => {
-  it('should have three message', () => {
+  it('should have three messages', () => {
     expect(store.listMessages().length).toBe(3);
   });
 
   it('should get message by message title', () => {
     let message = store.getMessage('zhufeng');
-    expect(message).toMatchObject({
+    expect(message).toEqual({
       title: 'zhufeng',
       content: 'zhufeng react'
     });
 
     message = store.getMessage('react');
-    expect(message).toMatchObject({
+    expect(message).toEqual({
       title: 'react',
       content: 'npm install react --save-dev'
     });
 
     message = store.getMessage('react-router');
-    expect(message).toMatchObject({
+    expect(message).toEqual({
       title: 'react-router',
       content: 'npm install react-router@3.0.0 --save-dev'
     });
